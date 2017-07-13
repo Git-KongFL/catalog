bd7135575f99502b5c530dc54a51905a9e075f61
denghaihui <denghaihui@192.168.3.14>
2017-07-13 13:41:15 +0800

    修改分类bug

--- src/main/java/com/vankle/catalog/service/impl/CalalogCategoryServiceImpl.java
+++ src/main/java/com/vankle/catalog/service/impl/CalalogCategoryServiceImpl.java
@@ -9,11 +9,13 @@ import com.vankle.catalog.dao.CatalogCategoryEntityMapper;
 import com.vankle.catalog.entity.CatalogCategoryEntity;
 import com.vankle.catalog.service.CalalogCategoryService;
 import com.vankle.code.constants.VankleConstants;
+import com.vankle.code.util.JsonDateValueProcessor;
 import com.vankle.code.util.JsonUtils;
 import com.vankle.code.util.VankleUtils;
 
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
+import net.sf.json.JsonConfig;
 
 /**
  * 
@@ -46,31 +48,38 @@ public class CalalogCategoryServiceImpl implements CalalogCategoryService {
 
 		JSONObject categoryObj = new JSONObject();
 		categoryObj = this.getCategoryByCategoryList(categoryObj, catalogCategoryEntitys);
-		
-		resultObj.put("catalogCategory", categoryObj);
-		//resultObj.put("catalogCategoryEntitys", JSONArray.fromObject(catalogCategoryEntitys) );
+		resultObj.put("data", categoryObj);
 		return resultObj.toString();
 	}
 	
+	
+	//递归生成类目列表
 	public JSONObject getCategoryByCategoryList(JSONObject obj,List<CatalogCategoryEntity> catalogCategoryEntitys){
-		System.out.println(obj.toString());
+		//System.out.println(obj.toString());
+		JsonConfig jsonConfig = new JsonConfig();  
+		jsonConfig.registerJsonValueProcessor(java.util.Date.class, new JsonDateValueProcessor());  
 		if(obj.get("id")==null){
 			for(CatalogCategoryEntity categoryEntity:catalogCategoryEntitys){
 				if(categoryEntity.getParentCategoryId()==null){
-					obj = JSONObject.fromObject(categoryEntity); 
+					obj = JSONObject.fromObject(categoryEntity,jsonConfig); 
+					JSONArray childs = new JSONArray();
+					obj.put("childs", childs);
 					this.getCategoryByCategoryList(obj, catalogCategoryEntitys);
 				}
 			}
 		}else{
 			for(CatalogCategoryEntity categoryEntity:catalogCategoryEntitys){
 				if(categoryEntity.getParentCategoryId()!=null&&obj.getInt("id")==categoryEntity.getParentCategoryId()){
-					JSONObject objChild = JSONObject.fromObject(categoryEntity); 
-					obj.put(objChild.get("id").toString(), objChild);
-					this.getCategoryByCategoryList(obj.getJSONObject(objChild.get("id").toString()), catalogCategoryEntitys);
+					JSONObject objChild = JSONObject.fromObject(categoryEntity,jsonConfig); 
+					JSONArray childsObj = new JSONArray();
+					objChild.put("childs", childsObj);
+					JSONArray childs = obj.getJSONArray("childs");
+					childs.add(objChild);
+					obj.put("childs", childs);
+					this.getCategoryByCategoryList(obj.getJSONArray("childs").getJSONObject(childs.size()-1), catalogCategoryEntitys);
 				}
 			}
 		}
-
 		return obj;
 	}
 	
