4eb6cb662e747e1f6fd37f6834437664b6c3d426
denghaihui <denghaihui>
2022-07-15 17:31:08 +0800

    productObj

--- src/main/java/com/vankle/catalog/service/impl/CalalogProductServiceImpl.java
+++ src/main/java/com/vankle/catalog/service/impl/CalalogProductServiceImpl.java
@@ -252,6 +252,7 @@ public class CalalogProductServiceImpl implements CalalogProductService {
 			
 			try {
 				logger.error(productObj.toString());
+				
 				JSONArray setList =productObj.getJSONArray("setList");
 				if(setList.getJSONObject(0).containsKey("value")) {
 					JSONArray valueList =setList.getJSONObject(0).getJSONArray("value");
